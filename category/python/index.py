675de7e7fc5dae4588100754dc880b83b939503b
sung <djsungkim@gmail.com>
2021-07-23 22:37:51 +0900

    pytyhon study

---
+++ category/python/index.py
@@ -0,0 +1,4 @@
+#!C:/Program Files/Python39/python.exe
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line, end of headers
+print('hello')
---
+++ category/python/study.py
@@ -0,0 +1,20 @@
+# string#1
+
+print("Hello Python")
+
+#escape
+print("hell'o'\"w\"orld")
+
+#newline
+print("hello\nworld")
+
+#docstring
+print('''
+hello
+world
+''')
+
+#string #2
+
+a = 'hello Pyhton'
+print(a)
